0c858bf7e39b6dd2da05e4f25cea4b94f0f8be2a
GRANDSOFT\huocy <huocy@glodon.com>
2022-08-12 16:36:17 +0800

    change

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -2,7 +2,7 @@
  * @Author: GRANDSOFT\huocy huocy@glodon.com
  * @Date: 2022-08-11 15:46:34
  * @LastEditors: GRANDSOFT\huocy huocy@glodon.com
- * @LastEditTime: 2022-08-12 16:23:39
+ * @LastEditTime: 2022-08-12 16:35:26
  * @FilePath: \cli\fast-cli-dev\packages\utils\lib\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,5 +11,5 @@
 module.exports = utils;
 
 function utils(a,b) {
-    return a + b + b;
+    return a + b + b +a;
 }
