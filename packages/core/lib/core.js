9763c1dc75ba242ac95f030bab034e1bf5c78f7a
GRANDSOFT\huocy <huocy@glodon.com>
2022-08-12 17:30:02 +0800

    test version

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -2,7 +2,7 @@
  * @Author: GRANDSOFT\huocy huocy@glodon.com
  * @Date: 2022-08-11 15:44:28
  * @LastEditors: GRANDSOFT\huocy huocy@glodon.com
- * @LastEditTime: 2022-08-12 16:52:17
+ * @LastEditTime: 2022-08-12 17:29:49
  * @FilePath: \cli\fast-cli-dev\packages\core\lib\core.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,6 @@
 module.exports = core;
 
 function core() {
-    console.log('test')
-    console.log('test')
+    console.log('version')
     return "Hello from core";
 }
